7f5fa16614d679b5c6d012c91550f87e7c9d1e60
KVISDAOWNER <krijen16@student.aau.dk>
2018-10-25 07:13:02 +0200

    Added RecordSpecs class in LiveSoundRecorder.py

--- LiveSoundRecorder.py
+++ LiveSoundRecorder.py
@@ -2,18 +2,26 @@ import SoundRecord as sr
 import os
 
 
-# Records and saves the samples in a stack like folder, deleting oldest sample before reaching the samples_kept
-def live_audio_rec(dir, samples_kept, sample_size):
+# Class containing info on sample duration, frame rate,
+class RecordSpecs:
+    def __init__(self, sample_secs=1, fs=44100, channels=1, samp_width=4):
+        self.sample_secs = sample_secs
+        self.fs = fs
+        self.ch = channels
+        self.sw = samp_width
+
+
+# Records and saves the samples in a queue like folder, deleting oldest sample before reaching the samples_kept
+def live_audio_rec(dir, samples_kept, r_spec):
     for i in range (0, samples_kept):
-        sr.rec(dir + "sample" + str(i) + ".wav", sample_size, 44100, 1, 4)
+        sr.rec(dir +"sample" + str(i) +".wav", r_spec.sample_secs, r_spec.fs, r_spec.ch, r_spec.sw)
     while True:
-        os.remove(dir + "sample0.wav")
+        os.remove(dir+"sample0.wav")
         for j in range (0, samples_kept-1):
-            print(dir + "sample" + str(j+1) + ".wav ->" + dir + "sample" + str(j) + ".wav")
-            os.rename(dir + "sample" + str(j+1) + ".wav", dir + "sample" + str(j) + ".wav")
-        print(dir + "sample" + str(samples_kept) + ".wav")
-        sr.rec(dir + "sample" + str(samples_kept-1) + ".wav", sample_size, 44100, 1, 4)
+            os.rename(dir+"sample"+str(j+1)+".wav", dir+"sample"+str(j)+".wav")
+        sr.rec(dir +"sample" + str(samples_kept-1) +".wav", r_spec.sample_secs, r_spec.fs, r_spec.ch, r_spec.sw)
 
 
 if __name__ == '__main__':
-    live_audio_rec('LiveSamples/', 4, 1)
+    record_specs = RecordSpecs()
+    live_audio_rec('LiveSamples/', 4, record_specs)
--- SoundRecord.py
+++ SoundRecord.py
@@ -2,12 +2,13 @@ import sounddevice as sd
 import wave
 
 
-def rec(filename, duration, fs, channels, sampwidth):
+# Records sound from mic, with the specifications given in parameters, and saves to wav type file
+def rec(filename, duration, fs, channels, samp_width):
     # Open/create the specified file in an overwrite mode
     wave_file = wave.open(filename, 'wb')
     wave_file.setframerate(fs)
     wave_file.setnchannels(channels)
-    wave_file.setsampwidth(sampwidth)
+    wave_file.setsampwidth(samp_width)
 
     # Actual recording, returns digital audio as an array of floats
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
