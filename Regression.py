2e8fc4fe237b75eb357171a5e71c6d3fbe414b59
Jacob6565 <jacob.gosch97@gmail.com>
2018-11-08 09:56:44 +0100

    Made so regression.py works

--- Regression.py
+++ Regression.py
@@ -1,73 +1,43 @@
-import numpy as N
-import matplotlib.pyplot as Plot
-from scipy.io import wavfile as W
-import specgram_maker as SM
-import xlsxwriter as Xlsx
+import os
+import specgram_maker
+import matplotlib.pyplot as plt
 FREQUENCY_INTEVALS = 442
-PATH = "C:\\Users\\Bjarke\\Desktop\\Universitet\\5.semester\\Lydeksempler\\"
-PATH2 = "C:\\Users\\Bjarke\\Desktop\\Universitet\\5.semester\\Dataset\\NoSiren\\"
 
 
 class Regression:
 
-    #function for writing data to Xlsx document
-    def filewrite(self, alist, t):
+    # function for extracting relevant data from a list of wavfiles.
+    def extract(self, path):
 
-        RegressionWorkbook = Xlsx.Workbook("Datapointsfourth.xlsx")
-
-        Worksheet = RegressionWorkbook.add_worksheet("Data for Regression")
-
-        alistLength = len(alist)
-
-        i = 0
-
-        while(i < alistLength):
-            Worksheet.write(i,1, alist[i])
-            Worksheet.write(i, 0, t[i])
-            i = i + 32
-
-        RegressionWorkbook.close()
-
-    #function for extracting relevant data from a list of wavfiles.
-    def extract(self, files, filenames):
-
-        columns = []
+        rows = []
         FilesFrequenciesAr = []
         time = []
-        sm = SM.SpecgramMaker()
-        l=0
-        for file in files:
-            spec, freq, t = sm.get_specgram_data_from_wav(PATH, filenames[l])
-            for i in range(len(spec[1])):
+        sm = specgram_maker.SpecgramMaker()
+
+        directory = os.listdir(path)
+        for filename in directory:
+            if not filename.endswith(".wav"):
+                continue
+            spec, freq, t = sm.get_specgram_data_from_wav(path, filename)
+            for i in range(len(spec[1])):  # iterating over coloums.
                 MaxFrequencyValue = 0
-                column = 0
-                for j in range(FREQUENCY_INTEVALS):
+                row = 0
+                for j in range(FREQUENCY_INTEVALS):  # Finding the row with highest frequency.
                     if spec[j][i] > MaxFrequencyValue:
                         MaxFrequencyValue = spec[j][i]
-                        column = j
-
-            #A.append(H) not in use
-                columns.append(50 * column)
-            FilesFrequenciesAr.append(columns)
-            columns.clear()
+                        row = j
+                rows.append(50 * row)
+            FilesFrequenciesAr.append(rows.copy())
+            rows.clear()
             time.append(t)
 
-            l += 1
         return FilesFrequenciesAr, time
 
 
-
-
-
-   # f = open("DataPoints.txt", "w+")
-
-    #filewrite(colomns, t)
-
-    """for i in range(len(t)):
-        f.write(str(t[i]) + ", " + str(colomns[i]) + "\n")
-
-    Plot.plot(t, colomns)
-    Plot.show()"""
-
-
-
+if __name__ == "__main__":
+    reg = Regression()
+    f, t = reg.extract("C:\\Users\\Jacob\\Music\\Samples\\Comp5\\")
+    for x in range(0, len(t)):
+        plt.plot(t[x],f[x])
+        plt.show()
+        a = input()
