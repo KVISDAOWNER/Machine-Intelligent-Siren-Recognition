90a27ae8b104bfe4dbf77460e26c0dbdc9108126
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2018-11-12 10:02:19 +0100

    Here is the newest version of the regression and SVM. The regression now prints how far it is with importing

--- Regression.py
+++ Regression.py
@@ -8,6 +8,9 @@ class Regression:
 
     # function for extracting relevant data from a list of wavfiles.
     def extract(self, path):
+        labels = []
+
+
 
         rows = []
         FilesFrequenciesAr = []
@@ -16,6 +19,10 @@ class Regression:
 
         directory = os.listdir(path)
         for filename in directory:
+            if "siren" in filename:
+                labels.append(True)
+            else:
+                labels.append(False)
             if not filename.endswith(".wav"):
                 continue
             spec, freq, t = sm.get_specgram_data_from_wav(path, filename)
@@ -30,7 +37,27 @@ class Regression:
             FilesFrequenciesAr.append(rows.copy())
             rows.clear()
             time.append(t)
+            print("Done importing " + filename + ".", labels[len(time) - 1], str(len(time) * 100 / len(directory)) + " %")
+        return FilesFrequenciesAr, time
 
+
+    def extract_single_file(self, filename):
+        sm = specgram_maker.SpecgramMaker()
+        rows = []
+        FilesFrequenciesAr = []
+        time = []
+        spec, freq, t = sm.get_specgram_data_from_wav("", filename)
+        for i in range(len(spec[1])):  # iterating over coloums.
+            MaxFrequencyValue = 0
+            row = 0
+            for j in range(FREQUENCY_INTEVALS):  # Finding the row with highest frequency.
+                if spec[j][i] > MaxFrequencyValue:
+                    MaxFrequencyValue = spec[j][i]
+                    row = j
+            rows.append(50 * row)
+        FilesFrequenciesAr.append(rows.copy())
+        rows.clear()
+        time.append(t)
         return FilesFrequenciesAr, time
 
 
@@ -38,6 +65,6 @@ if __name__ == "__main__":
     reg = Regression()
     f, t = reg.extract("C:\\Users\\Jacob\\Music\\Samples\\Comp5\\")
     for x in range(0, len(t)):
-        plt.plot(t[x],f[x])
+        plt.plot(t[x], f[x])
         plt.show()
         a = input()
--- SupportVectorMachine2.py
+++ SupportVectorMachine2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn.svm as svm
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
@@ -16,19 +17,41 @@ def get_training_data(directory):
     sm = SM.SpecgramMaker()
     r = R.Regression()
 
-    for filename in os.listdir(directory):
-        if filename == "Sirene21.wav":
+    _waves, freq = r.extract(directory)
+    for i in range(len(_waves)):
+        if "siren" in os.listdir(directory)[i]:
             labels.append(True)
-            sp, freq, t = sm.get_specgram_data_from_wav(directory, filename)
+        else:
+            labels.append(False)
+    return _waves
 
-            waves.append(sp)
-            print("Done importing from " + filename)
+
+def flatten_list(list_of_list):
+    for i in range(len(list_of_list)):
+        for j in range(len(list_of_list[i])):
+            flat_waves.append(list_of_list[i][j])
 
 
-def flatten_list(listlist):
-    for i in range(len(listlist)):
-        for j in range(len(listlist[i])):
-            flat_waves.append(listlist[i][j])
+def cut(list):
+    smallest_length = len(list[0])
+    result = []
+    for i in range(len(list)):
+        if len(list[i]) < smallest_length:
+            smallest_length = len(list[i])
+
+    for i in range(len(list)):
+        ls = []
+        for j in range(smallest_length):
+            ls.append(list[i][j])
+        result.append(ls)
+    return result
+
+
+def cut_to_size(input_list, size):
+    list = []
+    for i in range(size):
+        list.append(input_list[i])
+    return list
 
 
 def verify_list(list):
@@ -40,11 +63,26 @@ def verify_list(list):
     return True
 
 
+def get_data_from_file(path):
+    r = R.Regression()
+    data, freq = r.extract_single_file(path)
+    return data[0]
+
+
 if __name__ == "__main__":
-    get_training_data("Wav\\")
+    waves = get_training_data("C:\\Users\\Magnus\\Downloads\\TræningUnderLinjen\\")
     model = svm.SVC(kernel="linear")
+    LR_model = LogisticRegression()
+
     # flatten_list(waves)
     # print(verify_list(flat_waves))
+
+    waves = cut(waves)
+
     model.fit(waves, labels)
+    LR_model.fit(waves, labels)
     # accurately predict new data point
-    print("Done!")
+    verify_data = get_training_data("Wav\\")
+
+    predictions = model.predict(verify_data)
+    print(predictions)
