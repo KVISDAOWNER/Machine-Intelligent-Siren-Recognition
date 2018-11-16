9388685cd2912ab43f417e6f837d0460c34b7ac6
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2018-11-16 13:56:46 +0100

    Here is the newest versions of clipsplit.py and supportVectorMachine2.py

--- ClipSplit.py
+++ ClipSplit.py
@@ -2,13 +2,15 @@ import specgram_maker as SM
 import os
 
 
-def extract(path, max_freq=442):
+def extract(path, max_freq=442, length=20):
     sm = SM.SpecgramMaker()
     directory = os.listdir(path)
     time = []
     rows = []
     files_frequencies_array = []
     labels = []
+
+    # This for loop is the same as in regression
 
     for filename in directory:
         if not filename.endswith(".wav"):
@@ -28,41 +30,64 @@ def extract(path, max_freq=442):
         files_frequencies_array.append(rows.copy())
         rows.clear()
         time.append(t)
-        os.system("cls")
         print("Done importing " + filename + ".", labels[len(time) - 1], str(len(time) * 100 / len(directory)) + " %")
 
-    split_waves, labels = split_clips(files_frequencies_array, labels, directory, time)
+    # if length is 20, we split them with the _split_clips function
+    if length == 20:
+        split_waves, labels = _split_clips(files_frequencies_array, labels, directory, time)
+    # otherwise we do nothing
+    else:
+        split_waves = files_frequencies_array
 
     return split_waves, time, labels
 
 
-def find_subset_of_clip(clip, start, end, time):
+def _find_subset_of_clip(clip, start, end, time):
     index_one = -1
     index_two = -1
+    # index_one and index_two show on which index the clip begins and on which index the clip ends
     for i in range(len(time)):
         t = time[i]
+        # if we cross the timestamp given by start, we set index_one to i
         if (t >= start) and (index_one == -1):
             index_one = i
+        # if we cross the timestamp given by end, we set index_two to i
         if (t >= end) and (index_two == -1):
             index_two = i
-
+    # finally, we can return the part of the array that is between the indices of index_one and index_two
     return clip[index_one:index_two]
 
 
-def split_clips(files_frequencies_array, labels, file_names, time):
+def _split_clips(files_frequencies_array, labels, file_names, time):
     split_waves = []
     new_labels = []
-    for i in range(len(labels)):  # Assuming labels and files_frequencies_array are of equal length
+    for i in range(len(labels)):
+        # Assuming labels and files_frequencies_array are of equal length
         if labels[i]:
+            # If the i'th clip has a siren at some point
             siren_start = int(file_names[i].split("_")[3])
+            # The file names say when the sirens start. One filename could be Sample_2_sirenAt_13_Dogs_WWIISiren
+            # which would mean that the sirens start at the 13th second.
             for j in range(-3, 4):
+                # The idea is to find as many sub-clips in files_frequencies_array[i] as possible. To do this, we have
+                # to find out how many 5-second clips we can extract out of the 20 second clip. We only want clips that
+                # are 100 % sirens or 0 % sirens.
                 new_start = j * 5 + siren_start
+                # new_start is a potential starting second for a sub-clip.
                 if 0 <= new_start <= 15:
-                    split_waves.append(find_subset_of_clip(files_frequencies_array[i], new_start, new_start + 5, time[i]))
+                    # If new_start < 0 or new_start > 15, we skip it, because we only want 5-second clips.
+                    # Otherwise, we append a new clip to split_waves. The new clip is a five second clip between
+                    # new_start and new_start + 5.
+                    split_waves.append(_find_subset_of_clip(files_frequencies_array[i], new_start, new_start + 5,
+                                                            time[i]))
+                    # Then we append the new_labels with whether or not the siren has begun yet, or in other words,
+                    # whether the siren start is earlier than new_start
                     new_labels.append(new_start >= siren_start)
         else:
+            # If the clip does not have a siren in it at all, we split the 20-second clip into four 5-second clips
             for j in range(4):
-                split_waves.append(find_subset_of_clip(files_frequencies_array[i], 5 * j, 5 * j + 5, time[i]))
+                split_waves.append(_find_subset_of_clip(files_frequencies_array[i], 5 * j, 5 * j + 5, time[i]))
                 new_labels.append(False)
-
+    # split_waves are the frequencies split into 5-second clips.
+    # new_labels are the labels of the 5-second clips. The two lists should have equal length.
     return split_waves, new_labels
--- SupportVectorMachine2.py
+++ SupportVectorMachine2.py
@@ -8,12 +8,14 @@ from PIL import Image
 import Regression as R
 import specgram_maker as SM
 import ClipSplit as ClipSplit
+from sklearn.naive_bayes import GaussianNB
+from sklearn import linear_model
 
 waves = []
 
 
-def get_training_data(directory, max_freq=442):
-    _waves, time, labels = ClipSplit.extract(directory, max_freq)
+def get_training_data(directory, max_freq=442, length=20):
+    _waves, time, labels = ClipSplit.extract(directory, max_freq, length)
 
     return _waves, labels
 
@@ -43,24 +45,66 @@ def cut_to_size(list, length):
     return result
 
 
-def get_verification_data(path):
+def get_verification_data(path, max_freq=442):
     r = R.Regression()
 
-    _waves, time = r.extract(path)
+    _waves, time = r.extract(path, max_freq)
     return _waves
 
 
 if __name__ == "__main__":
     #  waves, labels = get_training_data("C:\\Users\\Magnus\\Downloads\\BGNLessThanOrEq\\")
-    waves, labels = get_training_data("Wav\\", 40)
-    model = svm.SVC(kernel="linear")
+    waves, labels = get_training_data("C:\\Users\\Magnus\\Desktop\\University\\5semester\\FirstHalf\\", 40)
+    svm_model = svm.SVC(kernel="linear")
+    bayes_model = linear_model.BayesianRidge()
+    nb_model = GaussianNB()
+    lr_model = LogisticRegression()
+
+    print("Begin cutting")
     waves = cut(waves)
 
-    model.fit(waves, labels)
-    # accurately predict new data point
-    verify_data = get_verification_data("C:\\Users\\Magnus\\Downloads\\TestSet5Seconds\\")
+    print("Begin fitting")
+    print("nb")
+    nb_model.fit(waves, labels)
+    # print("svm")
+    # svm_model.fit(waves, labels)
+    print("lr")
+    lr_model.fit(waves, labels)
+    print("bayes")
+    bayes_model.fit(waves, labels)
 
+    verify_data, actual_labels = get_training_data("C:\\Users\\Magnus\\Desktop\\University\\5semester\\SecondHalf\\",
+                                                   max_freq=40)
+
+    print("Cut verify data")
     verify_data = cut_to_size(verify_data, 291)
 
-    predictions = model.predict(verify_data)
-    print(predictions)
+    print("begin get predictions", "svm")
+    # svm_predictions = svm_model.predict(verify_data)
+    # print("nb")
+    nb_predictions = nb_model.predict(verify_data)
+    print("lr")
+    lr_predictions = lr_model.predict(verify_data)
+    print("bayes")
+    bayes_predictions = bayes_model.predict(verify_data)
+
+    svm_correct, nb_correct, lr_correct, samples, bayes_correct = 0, 0, 0, 0, 0
+
+    print("begin calculate accuracy")
+    for i in range(len(lr_predictions)):
+        # print("SVM:", svm_predictions[i], "NB:", nb_predictions[i], "LR:", lr_predictions[i],
+        # "actual value:", actual_labels[i])
+        # if svm_predictions[i] == actual_labels[i]:
+        #    svm_correct += 1
+        if nb_predictions[i] == actual_labels[i]:
+            nb_correct += 1
+        if lr_predictions[i] == actual_labels[i]:
+            lr_correct += 1
+        if bayes_predictions[i] == actual_labels[i]:
+            bayes_correct += 1
+        samples += 1
+
+    # print("svm accuracy:", 100 * svm_correct / samples)
+    print("NB accuracy:", 100 * nb_correct / samples)
+    print("LR accuracy:", 100 * lr_correct / samples)
+    print("Bayes accuracy", 100 * bayes_correct / samples)
