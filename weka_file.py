53707896d8bbf6b3da9fa82bc7515b9acd304636
KVISDAOWNER <krijen16@student.aau.dk>
2018-12-03 10:28:44 +0100

    weka_file.py clip split data functionality added

--- weka_file.py
+++ weka_file.py
@@ -1,13 +1,11 @@
 import csv
-import os
-from specgram_maker import SpecgramMaker
-from Regression import Regression
+import clip_split as cs
 
 
-def write_row_data_excl(filepath, rows):
+def _write_row_data_excl(filepath, rows):
     with open(filepath, "w", newline="\n") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        x=1
+        x = 1
         for row in rows:
             print("Writing row: " + str(x))
             x += 1
@@ -15,94 +13,39 @@ def write_row_data_excl(filepath, rows):
 
 
 # Generates metadata for arff type dataset
-def _arff_metadata(title, num_labels, class1, class2, label_type = "REAL"):
+def _arff_metadata(title, num_features, class1, class2, label_type = "REAL"):
     metadata = []
     # a1, a2, .., a_n label
     metadata.extend([["@RELATION {}".format(title)]])
-    for x in range(num_labels):
+    for x in range(num_features):
         metadata.extend([["@ATTRIBUTE a{} ".format(str(x)) + label_type]])
     metadata.extend([["@ATTRIBUTE class {{{}".format(class1), "{}}}".format(class2)]])
     metadata.extend([["@DATA"]])
     return metadata
 
 
-def extract_frequency_data(siren_dir, no_siren_dir, out, max_freq = 442):
-    e = Regression()
+def get_ver_max_data(data_dir, out, divisions=1):
+    # setup
     g = []
 
-    # extracting siren frequencies
-    print("Extracting siren spectrograms")
-    f, t = e.extract(siren_dir, 40)
+    # extracting features
+    print("Extracting features")
+    freq, time, lab = cs.extract(data_dir, divisions=divisions)
+    g.extend(_arff_metadata("sirendata", len(freq[0]), "siren", "nosiren"))
 
-    g.extend(_arff_metadata("siren", len(f[0]), "siren", "nosiren"))
-
-    for x in range(len(f)):
+    for x in range(len(freq)):
         g_list = []
-        g_list.extend(f[x])
-        g_list.append("siren")
+        g_list.extend(freq[x])
+        if lab[x]:
+            g_list.append("siren")
+        else:
+            g_list.append("nosiren")
         g.append(g_list)
-
-    print("Extracting nosiren spectrograms")
-    f, t = e.extract(no_siren_dir)
-
-    for x in range(len(f)):
-        g_list = []
-        g_list.extend(f[x])
-        g_list.append("nosiren")
-        g.append(g_list)
-    write_row_data_excl(out, g)
-
-
-def matrices_from_dir(path):
-    matrices = []
-    directory = os.listdir(path)
-    sm = SpecgramMaker()
-    for filename in directory:
-        print("Extracting data from " + filename)
-        if not filename.endswith(".wav"):
-            continue
-        spec, freq, t = sm.get_specgram_data_from_wav(path, filename)
-        matrices.append(spec)
-    return matrices
-
-
-def get_matrix_data(siren_dir, no_siren_dir, out):
-    ns_matrices = matrices_from_dir(no_siren_dir)
-    s_matrices = matrices_from_dir(siren_dir)
-    data_set = []
-
-    ns_row_length = len(ns_matrices[0])
-    ns_col_length = len(ns_matrices[0][0])
-
-    s_row_length = len(s_matrices[0])
-    s_col_length = len(s_matrices[0][0])
-
-    if ns_row_length != s_row_length or ns_col_length != s_col_length:
-        print("Data conversion ERROR: Datasets are not same size")
-        exit(-1)
-
-    data_set.extend(_arff_metadata("siren", ns_row_length * ns_col_length, "siren", "nosiren"))
-
-    for matrix in s_matrices:
-        data_set.append(_extract_data_row(matrix, "siren"))
-
-    for matrix in ns_matrices:
-        data_set.append(_extract_data_row(matrix, "nosiren"))
-
-    write_row_data_excl(out, data_set)
-
-
-def _extract_data_row(matrix, label):
-    cell_list = []
-    for row in matrix:
-        cell_list += row.tolist()
-    cell_list.append(label)
-    return cell_list
+    _write_row_data_excl(out, g)
 
 
 if __name__ == '__main__':
-    output_filename = "mf_teest_clean.arff"
-    s_dir = "C:\\Users\\kristoffer\\Desktop\\data\\teestclean\\siren\\"
-    ns_dir = "C:\\Users\\kristoffer\\Desktop\\data\\teestclean\\nosiren\\"
-    extract_frequency_data(s_dir, ns_dir, output_filename, max_freq=40)
-    # extract_data_csv(siren_dir, nosiren_dir, output_filename)
+    output_filename = "testtest.arff"
+    dir = "C:\\Users\\kristoffer\\Desktop\\Skrivebord\\training_data\\teest\\"
+    # To extract data with 5 splits
+    get_ver_max_data(dir, output_filename, 5)
